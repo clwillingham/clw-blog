9214247370938cdb07a0fa58347d098c6b628a2b
Chris Willingham <clwillingham@gmail.com>
2013-09-17 16:28:53 -0400

    ember stuff

---
+++ assets/js/app/controllers/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: chris
+ * Date: 9/17/13
+ * Time: 3:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+App.ApplicationController = Ember.Controller.extend({
+    title: "Chris's Programs and Projects",
+    description: ""
+});
